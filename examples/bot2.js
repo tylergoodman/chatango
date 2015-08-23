6cdb48fb79cc95c6adc95d84cee13c514e074064
tylergoodman <finlist@gmail.com>
2015-08-23 15:01:19 -0700

    changed debug logging - more more spam now

--- examples/bot2.js
+++ examples/bot2.js
@@ -3,8 +3,10 @@
 // var Chatango = require('..'); // VS Code can't find definitions if you do this :(
 var Chatango = require('../dist/index');
 var faker = require('faker');
+var winston = require('winston');
+winston.level = 'debug';
 
-var room = Chatango.joinRoom('streamerrant', 'ttttestuser', 'asdf1234');
+var room = Chatango.joinRoom('ttttest', 'ttttestuser', 'asdf1234');
 var user = room.user;
 
 room.on('message', function (message) {
